a632e976adeb4ffceb3592e99f95f8c79f758486
Alan Nascimento <alanvan3791@gmail.com>
2022-12-24 00:22:21 -0300

    variable renaming and logic improved

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 graph = []
-graph_size = 75
+GRAPH_SIZE = 100
 
 def create_x_y_lines():
-    for i in range(graph_size):
-        graph[graph_size//2][i] = '_'
-        graph[i][graph_size//2] = "|"
+    for i in range(GRAPH_SIZE):
+        graph[GRAPH_SIZE//2][i] = '_'
+        graph[i][GRAPH_SIZE//2] = "|"
 
 def create_graph():
-    for row in range(graph_size):
+    for row in range(GRAPH_SIZE):
         graph.append([])
-        for col in range(graph_size):
+        for col in range(GRAPH_SIZE):
             graph[row].append(' ')
     create_x_y_lines()
 
 def place_canvas(x,y):
-    if x < graph_size-1 and y < graph_size-1 and x+graph_size//2>0 and -y+graph_size//2>0:
-        graph[-y+graph_size//2][x+graph_size//2] = '#'
+    if x+GRAPH_SIZE//2>0 and -y+GRAPH_SIZE//2>0:
+        if x+GRAPH_SIZE//2<GRAPH_SIZE-1 and -y+GRAPH_SIZE//2<GRAPH_SIZE-1:
+            graph[-y+GRAPH_SIZE//2][x+GRAPH_SIZE//2] = '#'
 
 def place_function(a,b,c):
-    for x in range(-graph_size//2,graph_size//2):
+    for x in range(-GRAPH_SIZE//2,GRAPH_SIZE//2):
         y = a*x**2 + b*x + c
         place_canvas(int(x),int(y))
         print(f'placing at ({int(x)},{int(y)})')
